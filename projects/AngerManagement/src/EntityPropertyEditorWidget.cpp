b375044fb0ff554a0b369129f6ac260a0317d527
Derek Truong <cdtruong@usc.edu>
2017-02-15 00:33:10 -0800

    Setup the widget for the property editor

---
+++ projects/AngerManagement/src/EntityPropertyEditorWidget.cpp
@@ -0,0 +1,57 @@
+﻿#include "EntityPropertyEditorWidget.h"
+#include "EditorApplication.h"
+
+#include <Core/SimpleMath.h>
+
+EntityPropertyEditorWidget::EntityPropertyEditorWidget(QWidget* parent) : QWidget(parent)
+{
+	ui.setupUi(this);
+
+	connect(ui.XPosText, SIGNAL(textChanged(QString)), this, SLOT(UpdatePosition()));
+	connect(ui.YPosText, SIGNAL(textChanged(QString)), this, SLOT(UpdatePosition()));
+	connect(ui.ZPosText, SIGNAL(textChanged(QString)), this, SLOT(UpdatePosition()));
+
+	connect(ui.XRotText, SIGNAL(textChanged(QString)), this, SLOT(UpdateRotation()));
+	connect(ui.YRotText, SIGNAL(textChanged(QString)), this, SLOT(UpdateRotation()));
+	connect(ui.ZRotText, SIGNAL(textChanged(QString)), this, SLOT(UpdateRotation()));
+
+	connect(ui.scaleText, SIGNAL(textChanged(QString)), this, SLOT(UpdateScale()));
+}
+
+EntityPropertyEditorWidget::~EntityPropertyEditorWidget()
+{
+	
+}
+
+void EntityPropertyEditorWidget::UpdatePosition()
+{
+	MAD::Vector3 newPosition;
+
+	newPosition.x = ui.XPosText->text().toFloat();
+	newPosition.y = ui.YPosText->text().toFloat();
+	newPosition.z = ui.ZPosText->text().toFloat();
+
+	// Tell the engine proxy to update the selected entity's position
+}
+
+void EntityPropertyEditorWidget::UpdateRotation()
+{
+	float xRotRadians = MAD::ConvertToRadians(ui.XRotText->text().toFloat());
+	float yRotRadians = MAD::ConvertToRadians(ui.YRotText->text().toFloat());
+	float zRotRadians = MAD::ConvertToRadians(ui.ZRotText->text().toFloat());
+
+	MAD::Quaternion newRotation = MAD::Quaternion::CreateFromYawPitchRoll(yRotRadians, xRotRadians, zRotRadians);
+
+	newRotation;
+
+	// Tell the engine proxy to update the selected entity's rotation
+}
+
+void EntityPropertyEditorWidget::UpdateScale()
+{
+	float newScale = ui.scaleText->text().toFloat();
+
+	newScale;
+
+	// Tell the engine proxy to update the selected entity's scale
+}
---
+++ projects/AngerManagement/src/EntityPropertyEditorWidget.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <QWidget>
+#include "ui_entitypropertyeditorwidget.h"
+
+
+class EntityPropertyEditorWidget : public QWidget
+{
+	Q_OBJECT
+public:
+	EntityPropertyEditorWidget(QWidget* parent = Q_NULLPTR);
+	~EntityPropertyEditorWidget();
+
+private slots:
+	
+	void UpdatePosition();
+	void UpdateRotation();
+	void UpdateScale();
+private:
+	Ui::EntityPropertyEditorWidget ui;
+};
