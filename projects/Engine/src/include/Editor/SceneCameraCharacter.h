aaf0c722f30751742543bf424f7740eeb446fab2
Derek Truong <cdtruong@usc.edu>
2017-03-14 20:46:53 -0700

    Removed some unnecessary comments from SceneCameraCharacter

--- projects/Engine/src/include/Editor/SceneCameraCharacter.h
+++ projects/Engine/src/include/Editor/SceneCameraCharacter.h
@@ -9,20 +9,7 @@ namespace MAD
 		MAD_DECLARE_ACTOR(ASceneCameraCharacter, AEntity)
 	public:
 		explicit ASceneCameraCharacter(OGameWorld* inOwningWorld);
-
-		/*virtual void OnBeginPlay() override;
-	private:
-		void Move(const Vector3& inDelta);
-		void Look(const Quaternion& inDelta);
-		void MoveRight(float inVal);
-		void MoveForward(float inVal);
-		void MoveUp(float inVal);
-		void LookRight(float inVal);
-		void LookUp(float inVal);*/
 	private:
 		eastl::shared_ptr<class CCameraComponent> m_cameraComponent;
-
-		//float m_lookSpeed;
-		//float m_moveSpeed;
 	};
 }
--- projects/Engine/src/private/Editor/SceneCameraCharacter.cpp
+++ projects/Engine/src/private/Editor/SceneCameraCharacter.cpp
@@ -5,8 +5,6 @@ namespace MAD
 {
 	ASceneCameraCharacter::ASceneCameraCharacter(OGameWorld* inOwningWorld)
 		: Super_t(inOwningWorld)
-		//, m_lookSpeed(1.0f)
-		//, m_moveSpeed(300.0f)
 	{
 		m_cameraComponent = AddComponent<CCameraComponent>();
 		SetRootComponent(m_cameraComponent);
